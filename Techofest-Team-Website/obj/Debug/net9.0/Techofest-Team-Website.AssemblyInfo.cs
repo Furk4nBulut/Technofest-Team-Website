3a98ea23e5c321db170025325aab5e15a8b34b83
Furkan Bulut <furkanbtng@gmail.com>
2025-05-01 23:24:52 +0300

    Index html updated

--- Techofest-Team-Website/obj/Debug/net9.0/Techofest-Team-Website.AssemblyInfo.cs
+++ Techofest-Team-Website/obj/Debug/net9.0/Techofest-Team-Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Techofest-Team-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d80dd086fd2f550cd1d8fb42e35def171d7bb196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ce9ee187531fc869c6563edd40bf06ab083dba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Techofest-Team-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Techofest-Team-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
