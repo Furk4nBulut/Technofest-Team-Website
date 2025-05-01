4e66edbf8e5a0fb3d78991f96fbdd5f162625b24
Furkan Bulut <furkanbtng@gmail.com>
2025-05-01 20:42:31 +0300

    Logout button fixed changed with a form

--- Techofest-Team-Website/obj/Debug/net9.0/Techofest-Team-Website.AssemblyInfo.cs
+++ Techofest-Team-Website/obj/Debug/net9.0/Techofest-Team-Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Techofest-Team-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478946a14853513bc6b2bab30a8a081e459af36b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f0d76a044bf584151e6d892946dcbacb10b52f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Techofest-Team-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Techofest-Team-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
